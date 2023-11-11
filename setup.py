9674d78c64ade3673d4a330dd8a0568d2eaaaad4
Harcic <harcic@outlook.com>
2023-11-11 13:28:21 +0800

    Updated readme.md

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="rev_hunyuan",
-    version="0.1",
+    version="0.2",
     description="通过cookie信息快速访问腾讯混元",
     author="Harcic",
     author_email="harcic@outlook.com",
